2d26777f8416bbb275002973c0979a7ebf579ba4
dcaon01 <d.caon01@gmail.com>
2025-09-20 18:21:37 +0200

    Button implemented.

--- components/atoms/button/SIButton.tsx
+++ components/atoms/button/SIButton.tsx
@@ -1,10 +1,12 @@
+import classes from './SIButton.module.scss';
+
 type Props = {
     text: string;
 }
 
 export default function SIButton({ text }: Props) {
   return (
-    <button className="si-button">
+    <button className={classes.SIButton}>
       {text}
     </button>
   )
