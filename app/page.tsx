1b136c56ad0879a2e1b2665e46574a5005de5be9
dcaon01 <d.caon01@gmail.com>
2025-09-28 22:58:18 +0200

    Motion installed, desktop navbar setupped.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-import styles from "./page.module.css";
+import classes from "./page.module.scss";
+import "./globals.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="main">
+      <div className={classes.heroSection}>
+        <img src="/hero-image.jpg" alt="Hero Image" className={classes.heroImage} />
+        <div className={classes.heroTextContainer}>
+          <p className={classes.heroQuote}>
+            “L’architettura 
+            è il gioco sapiente,
+            rigoroso e magnifico
+            dei volumi assemblati
+            nella luce”
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
--- components/molecules/navbar/DesktopNavbar.tsx
+++ components/molecules/navbar/DesktopNavbar.tsx
@@ -1,5 +1,64 @@
+'use client'
+
+import { AnimatePresence, motion } from "motion/react";
+import { ChevronDown } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import classes from "./DestopNavbar.module.scss";
+
+/**
+ * The desktop version of the navigation bar.
+ */
 export default function DesktopNavbar() {
+  /** State to manage dropdown visibility */
+  const [isDroppedDown, setIsDroppedDown] = useState(false)
+
+  /**
+   * Toggles the dropdown menu visibility.
+   */
+  function manageDropdown() {
+    setIsDroppedDown(!isDroppedDown);
+  }
+
   return (
-    <div></div>
+    <div className={classes.navbarContainer}>
+      {/* Logo Section */}
+      <img src="/better-logo.svg" alt="Logo" className={classes.logo} />
+
+      {/* Main Navigation Links */}
+      <div className={classes.navLinks}>
+        <Link href="/" className={classes.link} onClick={manageDropdown}>Home</Link>
+
+        {/* Dropdown Menu for Projects */}
+        <div className={classes.dropdownLinks} onClick={manageDropdown}>
+          <p className={classes.link}>Progetti</p>
+          <motion.div>
+            <ChevronDown className={classes.dropdownIcon} />
+          </motion.div>
+          <AnimatePresence>
+            {
+              isDroppedDown && (
+                <motion.div
+                  className={classes.dropdownContent}
+                  initial={{ top: 20, opacity: 0 }}
+                  animate={{ top: 30, opacity: 1 }}
+                  exit={{ top: 30, opacity: 0 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <Link href="/projects" className={classes.link} onClick={manageDropdown}>Tutti</Link>
+                  <Link href="/projects?filter=1" className={classes.link} onClick={manageDropdown}>Filtro1</Link>
+                  <Link href="/projects?filter=2" className={classes.link} onClick={manageDropdown}>Filtro2</Link>
+                  <Link href="/projects?filter=3" className={classes.link} onClick={manageDropdown}>Filtro3</Link>
+                </motion.div>
+              )
+            }
+          </AnimatePresence>
+        </div>
+
+        <Link href="/profile" className={classes.link} onClick={manageDropdown}>Profilo</Link>
+        <Link href="/contacts" className={classes.link} onClick={manageDropdown}>Contatti</Link>
+      </div>
+    </div>
   )
 }
+
